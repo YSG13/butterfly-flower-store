69805e13a6b38393f42733938ce26249781d51eb
Yousif Ahmed <ysg@Yousifs-MacBook-Pro.local>
2025-06-04 09:32:43 +0300

    initial

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+console.log("APP IS RENDERING!");
+
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Hello Butterfly 🦋</h1>
+      <p>If you're seeing this, it's working!</p>
+    </div>
+  );
+}
